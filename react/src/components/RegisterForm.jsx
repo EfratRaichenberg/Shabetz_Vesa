860c3af7ea4232fbd68e208789652449b74e356c
EfratRaichenberg <efrat7024@gmail.com>
2021-11-17 16:58:05 +0200

    change_registerForm

--- react/src/components/RegisterForm.jsx
+++ react/src/components/RegisterForm.jsx
@@ -1,84 +1,111 @@
-import { useState } from "react";
+import React, { useState , useEffect } from 'react';
 import ReactDOM from 'react-dom';
+import axios  from 'axios';
 
 function RegisterForm() {
-    const [firstName, setfirstName] = useState("");
-    const [lastName, setlastName] = useState("");
-    const [street, setStreet] = useState("");
-    const [neighborhood, setNeighborhood] = useState("");
-    const [city, setCity] = useState("");
-    const [mail, setMail] = useState("");
-    const [phone, setPhone] = useState("");
-
-
+    const [passengerState , setPassengerState] = useState(
+        {
+           firstName : '',
+           lastName : '',
+           street : '',
+           neighborhood : '',
+           city : '',
+           Hospital : '',
+           phone : '',
+           Hour:''
+        }
+    );
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        alert('${firstName} sent a registery form')
-    }
-
+        axios.post(`http://localhost:3001/passenger/addNewPassenger`,
+            {
+                "Name" : passengerState.firstName+" "+passengerState.lastName,
+                "Phone_number" :passengerState.phone,
+                "City" : passengerState.city,
+                "Neighborhood" : passengerState.neighborhood,
+                "Street" : passengerState.street,
+                "Hospital":passengerState.Hospital,
+                "Hour" : passengerState.Hour
+                
+            })
+            .then(response => response.status)
+            .catch(err => console.warn(err));
+            alert("insert successfuly");
+        };
+    //     axios.get(`http://localhost:3001/volunteer/getVolunteerList`)
+    // .then(res=>{
+    //   alert(JSON.stringify(res.data))
+    // });
     return (
         <form onSubmit={handleSubmit}>
             <label>First name:
                 <input
                     type="text"
-                    value={firstName}
-                    onChange={(e) => setfirstName(e.target.value)}
+                    value={passengerState.firstName}
+                    onChange={(e)=> setPassengerState({...passengerState, firstName: e.target.value})}
                 />
             </label>
             <br />
             <label>Last name:
                 <input
                     type="text"
-                    value={lastName}
-                    onChange={(e) => setlastName(e.target.value)}
+                    value={passengerState.lastName}
+                    onChange={(e)=> setPassengerState({...passengerState, lastName: e.target.value})}
                 />
             </label>
             <br />
             <label>street:
                 <input
                     type="text"
-                    value={street}
-                    onChange={(e) => setStreet(e.target.value)}
+                    value={passengerState.street}
+                    onChange={(e)=> setPassengerState({...passengerState, street: e.target.value})}
                 />
             </label>
             <br />
             <label>neighborhood:
                 <input
                     type="text"
-                    value={neighborhood}
-                    onChange={(e) => setNeighborhood(e.target.value)}
+                    value={passengerState.neighborhood}
+                    onChange={(e)=> setPassengerState({...passengerState, neighborhood: e.target.value})}
                 />
             </label>
             <br />
             <label>city:
                 <input
                     type="text"
-                    value={city}
-                    onChange={(e) => setCity(e.target.value)}
+                    value={passengerState.city}
+                    onChange={(e)=> setPassengerState({...passengerState, city: e.target.value})}
                 />
             </label>
             <br />
-            <label>mail:
+            <label>Hospital:
                 <input
                     type="text"
-                    value={mail}
-                    onChange={(e) => setMail(e.target.value)}
+                    value={passengerState.Hospital}
+                    onChange={(e)=> setPassengerState({...passengerState, Hospital: e.target.value})}
                 />
             </label>
             <br />
             <label>phone:
                 <input
                     type="text"
-                    value={phone}
-                    onChange={(e) => setPhone(e.target.value)}
+                    value={passengerState.phone}
+                    onChange={(e)=> setPassengerState({...passengerState, phone: e.target.value})}
                 />
             </label>
             <br />
+            <label>hour:
+                <input
+                    type="text"
+                    value={passengerState.Hour}
+                    onChange={(e)=> setPassengerState({...passengerState, Hour: e.target.value})}
+                />
+            </label>
             <input type="submit" />
         </form>
     )
 }
 
 ReactDOM.render(<RegisterForm />, document.getElementById('root'));
-export default RegisterForm
+export default RegisterForm;
